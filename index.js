0711468fb12556c3cf02b16d4d636804241582f2
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-15 08:02:08 -0600

    install passport and specify google-oauth20 strategy

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const express = require('express'); // Get access to the express library
-const app = express(); // Generate a running express app
+// Get access to the express library
+const express = require('express'); 
+
+// Helpers for handling auth in express app
+const passport = require('passport'); 
+
+// Specific method for authenticating
+const GoogleStrategy = require('passport-google-oauth20').Strategy; 
+const keys = require('./config/keys');
+
+// Generate a running express app
+const app = express(); 
+
+// Set configuration for google strategy
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        console.log('User authenticated');
+        console.log('accessToken:', accessToken);
+        console.log('refreshToken:', refreshToken);
+        console.log('profile:', profile);
+    })
+);
 
 // Example of route handler:
 // app.get      -> Get info 
@@ -7,11 +32,19 @@ const app = express(); // Generate a running express app
 //    .put      -> Update all the properties of something
 //    .delete   -> Delete something
 //    .patch    -> Update one or two properties of something
-app.get('/', (req, res) => {
-    res.send({bye: 'friend'});
-});
+
+// Go and find google strategy
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email'] 
+    })
+);
+
+// Handle oauth callback
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Set port number injected in the environment (Production / Development)
 const PORT = process.env.PORT || 5001; 
 
-app.listen(PORT); // Instruct express to tell node to listen to port:5001
+// Instruct express to tell node to listen to port:5001
+app.listen(PORT); 
