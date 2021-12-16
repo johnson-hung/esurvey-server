91a750eabfdb1238de2193b9036230e979c7cfd9
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-15 21:20:19 -0600

    wire up oauth flow by enabling cookie

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Get access to the express library
 const express = require('express');
-
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
@@ -11,6 +12,16 @@ mongoose.connect(keys.mongoURI);
 
 // Generate a running express app
 const app = express(); 
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in [ms]
+        keys: [keys.cookieKey] // Pick keys randomly
+    })
+);
+// Tell passport to use cookie to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // 'require' returns a function, and we immediately call it with the app object
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,8 @@
 const passport = require('passport'); 
 
 // Specific method for authenticating
+// (Remember that OAuth's only purpose is to allow someone to sign in.
+// After that, we use our own internal id)
 const GoogleStrategy = require('passport-google-oauth20').Strategy; 
 const keys = require('../config/keys');
 
@@ -10,6 +12,18 @@ const mongoose = require('mongoose');
 // Model class -> collection
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    // user.id is not the same as profile.id, it's a shortcut to the ID
+    // assigned to a record by MongoDB
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 // Set configuration for google strategy
 passport.use(
     new GoogleStrategy({
@@ -17,7 +31,7 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, 
-    (accessToken, refreshToken, profile, done) => {
+    (accessToken, refreshToken, profile, done) => { // Google strategy callback function
         // Make query to MongoDB (asynchronous, returns a promise)
         User.findOne({ googleID: profile.id }).then((existingUser) => {
             if (existingUser){
