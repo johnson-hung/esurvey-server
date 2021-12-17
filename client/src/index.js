cb1608a45ac98e4f3dce0b044b2f4778c4af62ad
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-17 11:06:06 -0600

    add authReducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App'
+import reducers from './reducers';
 
 // Set up redux store which stores all the states
-const store = createStore(()=> [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, // Hook up provider and redux store
