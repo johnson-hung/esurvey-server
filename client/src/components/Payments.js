72dcb4e7921acef7fbd0152f08337e52e614876f
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-19 11:03:23 -0600

    fix payment component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,14 @@ class Payments extends Component {
     render(){
         return(
             <StripeCheckout 
+                name="E-Survey"
+                description="$5.00 for 5 email credits"
                 amount={500}
                 token={token => console.log(token)} // Token received from Stripe
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">Add Credits</button>
+            </StripeCheckout>
         );
     }
 }
