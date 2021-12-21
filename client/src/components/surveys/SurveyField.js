668a4ebb986895bcd5f3ff6b375087f30ae9d70d
johnson-hung <jhcc.gamedev@gmail.com>
2021-12-21 11:46:03 -0600

    add survey field component

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+// SurveyField.js - renders single label and text input
+
+import React from 'react';
+
+export default ({ input, label }) => {
+    return (
+        <div>
+            <label>{ label }</label>
+            <input {...input}/>
+        </div>
+    );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,41 @@
 // SurveyForm.js - shows a form for user to add input
 
+import _ from 'lodash';
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 // Allows components to communicate with the redux store
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title'},
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email body', name: 'body'},
+    { label: 'Recipient List', name: 'emails'}
+];
 
 class SurveyForm extends Component{
+    renderField(){
+        return _.map(FIELDS, ({ label, name }) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        });
+    }
+
     render() {
         return(
             <div>
-                SurveyForm
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderField()}
+                    <Link to="/surveys" className="grey btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">navigate_next</i>
+                    </button>
+                </form>
             </div>
         );
     }
